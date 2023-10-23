3b1f3cbf9cb2b7d31a7ad137c063ca6d0c4c3ecf
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-10-24 04:47:21 +0530

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
+import profile from "../assets/AbhinavProfile.jpg"
 
-const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
 
 const Footer = () => {
   return (
@@ -21,12 +21,12 @@ const Footer = () => {
             textAlign={["center", "left"]}
           >
             We are the best crypto trading app in India, we provide our guidance
-            at a very cheap price.
+            at a very affordable price.
           </Text>
         </VStack>
 
         <VStack>
-          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={profile} />
           <Text>Our Founder</Text>
         </VStack>
       </Stack>
